e58e2a5ada7c30d8ac8dcaba04f2d4bc2f7f6d88
nikiton <nikiton@barabakens.local>
2017-02-20 23:19:15 +0200

    not first commit

--- src/VladIsTheBest.java
+++ src/VladIsTheBest.java
@@ -1,6 +1,6 @@
 
 public class VladIsTheBest {
 	public static void main(args [] a){
-	System.out.println("JAVA YEEEEEEEEEEEH");
+	System.out.println("JAVA YEEEEEEEEEEEH NO PYTHON YEEEHEE”);
 	} 		
 }
