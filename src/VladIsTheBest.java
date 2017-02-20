51ff06274d155a73176d87ec44aa1769395b8030
VLad <pantiushenkov@gmail.com>
2017-02-20 23:22:31 +0200

    no python

--- src/VladIsTheBest.java
+++ src/VladIsTheBest.java
@@ -1,6 +1,6 @@
 
 public class VladIsTheBest {
 	public static void main(args [] a){
-	System.out.println("JAVA YEEEEEEEEEEEH NO PYTHON YEEEHEE”);
+		System.out.print("JAVA YEEEEEEEEEEEH NO PYTHON YEEEHEE”);
 	} 		
 }
